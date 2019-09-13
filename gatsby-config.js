0407c046f7b52446e5e75187605fe841cf769f68
Paulo Tejada <paulo.tejada@MacBook-Pro-de-admin.local>
2019-09-13 09:58:25 -0500

    weoking ons tyles

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,7 +31,12 @@ module.exports = {
 		`gatsby-transformer-sharp`,
 		`gatsby-plugin-sharp`,
 		`gatsby-plugin-sass`,
-		'gatsby-plugin-styled-components',
+		{
+			resolve: `gatsby-plugin-styled-components`,
+			options: {
+			  // Add any options here
+			},
+		  },
 		{
 			resolve: `gatsby-plugin-manifest`,
 			options: {
