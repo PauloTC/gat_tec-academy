6ea18d3683c43d780f9f267a88bf8e9657be7705
Paulo Tejada <paulo.tejada@MacBook-Pro-de-admin.local>
2019-09-16 15:05:03 -0500

    working on styles

--- src/components/mobcarousel.js
+++ src/components/mobcarousel.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import MobileStepper from '@material-ui/core/MobileStepper';
 import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
 import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 import SwipeableViews from 'react-swipeable-views';
 import { autoPlay } from 'react-swipeable-views-utils';
+import { 
+  Typography,
+  Container,
+  Box
+} from '@material-ui/core';
 
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
@@ -42,13 +46,37 @@ const tutorialSteps = [
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
+    position: 'relative'
+  },
+  steps: {
+    position: 'absolute',
+    width: '100%',
+    top: '50%',
+    transform: 'translateY(-50%)',
+    zIndex: 10,
+    background: 'transparent'
   },
   header: {
     display: 'flex',
     alignItems: 'center',
-    height: 50,
+    height: '100%',
+    width: '100%',
+    zIndex: 9,
     paddingLeft: theme.spacing(4),
-    backgroundColor: theme.palette.background.default,
+    backgroundColor: 'rgba(0, 0, 0, 0.4)',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0
+  },
+  button: {
+    textTransform: "capitalize",
+    color: "white"
+  },
+  title: {
+    color: '#fff',
+    maxWidth: 600
   },
   img: {
     height: 255,
@@ -81,8 +109,15 @@ function SwipeableTextMobileStepper() {
 
   return (
     <div className={classes.root}>
-      <Paper square elevation="0" className={classes.header}>
-        <Typography>{tutorialSteps[activeStep].label}</Typography>
+      <Paper square elevation={0} className={classes.header}>
+        <Container maxWidth="md"  >
+          <Typography  >
+              <Box mb={2} className={classes.title} fontSize="h4.fontSize" fontWeight={800} >{tutorialSteps[activeStep].label}</Box>
+            </Typography>
+          <Button variant="contained" color="primary" className={classes.button}>
+              Inscribete ahora
+          </Button>
+        </Container>
       </Paper>
       <AutoPlaySwipeableViews
         axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
@@ -103,16 +138,15 @@ function SwipeableTextMobileStepper() {
         position="static"
         variant="text"
         activeStep={activeStep}
+        className={classes.steps}
         nextButton={
-          <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1}>
-            Next
+          <Button size="small" className={classes.button} onClick={handleNext} disabled={activeStep === maxSteps - 1}>
             {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
           </Button>
         }
         backButton={
-          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+          <Button size="small" className={classes.button} onClick={handleBack} disabled={activeStep === 0}>
             {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
-            Back
           </Button>
         }
       />
