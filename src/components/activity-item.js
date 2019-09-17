05af6d6f7b6c074059edf28a88f1796876a6355e
Paulo Tejada <paulo.tejada@MacBook-Pro-de-admin.local>
2019-09-17 15:20:17 -0500

    talleres complete

--- src/components/activity-item.js
+++ src/components/activity-item.js
@@ -22,6 +22,23 @@ const useStyles = makeStyles({
     card: {
         height: 305,
         backgroundSize: 'contain'
+    },
+    container: {
+        boxShadow : "1px 1px 16px 0 rgba(50, 16, 99, 0.1)"
+    },
+    flexible: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: 15
+    },
+    text: {
+        width: "70%",
+    },
+    btncontainer: {
+        width: "30%",
+        display: "flex",
+        justifyContent: "flex-end",
+        alignContent: "flex-end"
     }
 });
 
@@ -30,27 +47,20 @@ const ActivityItem = () => {
     const classes = useStyles();
     return (
         <Fragment>
-                <Card >
-                    <CardMedia className={classes.card}  image="http://tinyimg.io/i/tSFDV33.jpeg" />
-                    <Grid container  alignItems="flex-end"  >
-                        <Grid item >
-                            <CardContent>
-                                <Typography> 
-                                    <Box mb={1}  fontSize="subtitle2.fontSize">mar., 10 sept.</Box>
-                                    <Box lineHeight={1.2} mb={1}   fontSize="h6.fontSize" fontWeight="fontWeightBold" > Meetup: Time Management Skills </Box>
-                                    <Box  fontSize="body2.fontSize" > Bruno Díaz </Box>
-                                </Typography>
-                            </CardContent>
-                        </Grid>
-                        <Grid item >
-                            <Box mb={2} >
-                                <CardActions>
-                                        <Button className={classes.button} size="small" variant="outlined" color="primary"> Unirme </Button>
-                                </CardActions>
-                            </Box>
-                        </Grid>
-                    </Grid>
-                </Card>
+                <div  className={ classes.container }  >
+                    <img  width={430}  src="http://tinyimg.io/i/tSFDV33.jpeg"  />
+                    {/* <CardMedia className={classes.card}    image="" /> */}
+                    <div  className={ classes.flexible  } >
+                        <Typography className={ classes.text  } > 
+                            <Box mb={1}  fontSize="subtitle2.fontSize">mar., 10 sept.</Box>
+                            <Box lineHeight={1.2} mb={1}   fontSize="h6.fontSize" fontWeight="fontWeightBold" > Meetup: Time Management Skills </Box>
+                            <Box  fontSize="body2.fontSize" > Bruno Díaz </Box>
+                        </Typography>
+                        <Box className={ classes.btncontainer  }  >
+                            <Button className={classes.button} size="medium" variant="outlined" color="primary"> Unirme </Button>
+                        </Box>
+                    </div>
+                </div>
         </Fragment>
     )
 }
