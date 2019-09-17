55f0bb7c71ec8a3eff839e7f538a774c550c6206
Paulo Tejada <paulo.tejada@MacBook-Pro-de-admin.local>
2019-09-17 12:04:38 -0500

    working on styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
 import React , { Fragment }from 'react';
 import SEO from '../components/seo';
 
-import {  Container, Box, Grid } from '@material-ui/core';
+import {  Container, Box, Grid, Typography, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+
 // import Styles from '../components/index.module.scss';
 
 import Layout from '../components/layout';
@@ -13,9 +15,16 @@ import GroupCard from '../components/group-card'
 import { useStaticQuery, graphql } from 'gatsby';
 
 // import SwipeableTextMobileStepper  from '../components/mobcarousel'
+const useStyles = makeStyles({
+	button: {
+		textTransform: 'capitalize',
+		fontSize: '16px',
+		fontWeight: 900
+	}
+});
 
 const IndexPage = () => {
-	
+	const classes = useStyles();	
 	return (
 		<Fragment>
 			<Layout>
@@ -25,11 +34,23 @@ const IndexPage = () => {
 				</Box> */}
 				<Container maxWidth="md">
 					
-					<SubHeader  
+					{/* <SubHeader  
 							title="Próximos eventos"  
 							subtitle="Descubre lo que pasará proximamente en Belcorp"  
-							button="Ver talleres"  ></SubHeader>
-					
+							button="Ver talleres"  ></SubHeader> */}
+					<Fragment>
+						<Typography>
+							<Grid  container justify="space-between" >
+								<Grid  container item direction="column"   md={10} >
+									<Box fontWeight={900} fontSize="h6.fontSize" > Próximos eventos</Box>
+									<Box fontSize="body2.fontSize" > Descubre lo que pasará proximamente en Belcorp</Box>
+								</Grid>
+								<Grid container alignItems="center" justify="flex-end"  item md={2} >
+									<Button fontWeight={900} className={classes.button}  color="primary"> Ver talleres </Button>
+								</Grid>
+							</Grid>
+						</Typography>
+					</Fragment>
 					{/* <Box my={4}>
 						<Grid  container justify="space-between" spacing={2}  >
 							{	
@@ -38,11 +59,23 @@ const IndexPage = () => {
 						</Grid>
 					</Box> */}
 
-					<SubHeader  
+					{/* <SubHeader  
 							title="Próximas actividades"  
 							subtitle="¿Pizza Nights? ¿Meetups? Descubre todas las actividades aquí."  
-							button="Ver actividades"  ></SubHeader>
-
+							button="Ver actividades"  ></SubHeader> */}
+					<Fragment>
+						<Typography>
+							<Grid  container justify="space-between" >
+								<Grid  container item direction="column"   md={10} >
+									<Box fontWeight={900} fontSize="h6.fontSize" > Próximas actividades </Box>
+									<Box fontSize="body2.fontSize" > ¿Pizza Nights? ¿Meetups? Descubre todas las actividades aquí.</Box>
+								</Grid>
+								<Grid container alignItems="center" justify="flex-end"  item md={2} >
+									<Button fontWeight={900} className={classes.button}  color="primary"> Ver actividades</Button>
+								</Grid>
+							</Grid>
+						</Typography>
+					</Fragment>
 					{/* <Box my={4} >
 						<Grid  container justify="space-between" spacing={5}  >
 							
@@ -52,11 +85,24 @@ const IndexPage = () => {
 						</Grid>
 					</Box> */}
 					
-					<SubHeader  
+					{/* <SubHeader  
 						title="Puntajes Tec Valley "  
 						subtitle="¿Estás cerca a ganar ese after office?  ¡Aprovecha al máximo TecAcademy!"  
-						button="Ver puntajes"  ></SubHeader>
+						button="Ver puntajes"  ></SubHeader> */}
 
+					<Fragment>
+						<Typography>
+							<Grid  container justify="space-between" >
+								<Grid  container item direction="column"   md={10} >
+									<Box fontWeight={900} fontSize="h6.fontSize" > Puntajes Tec Valley  </Box>
+									<Box fontSize="body2.fontSize" > ¿Estás cerca a ganar ese after office?  ¡Aprovecha al máximo TecAcademy!</Box>
+								</Grid>
+								<Grid container alignItems="center" justify="flex-end"  item md={2} >
+									<Button fontWeight={900} className={classes.button}  color="primary"> Ver puntajes </Button>
+								</Grid>
+							</Grid>
+						</Typography>
+					</Fragment>
 					{/* <Box position="relative" my={4} >
 						<Grid  container spacing={3}  >	
 							{
