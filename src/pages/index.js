4f6e7465ed29fe18414a15ae5c3f159ed8bef474
Paulo Tejada <paulo.tejada@MacBook-Pro-de-admin.local>
2019-09-17 11:29:21 -0500

    working ons tyles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React , { Fragment }from 'react';
 import SEO from '../components/seo';
 
 import {  Container, Box, Grid } from '@material-ui/core';
-import Styles from '../components/index.module.scss';
+// import Styles from '../components/index.module.scss';
 
 import Layout from '../components/layout';
 import HomeCourse from '../components/home-course/home-course';
@@ -59,54 +59,57 @@ const IndexPage = () => {
 		}
 	`);
 	return (
-		<Layout>
-			<SEO title="Inicio" />
-			{/* <Box mb={4}>
-				<SwipeableTextMobileStepper></SwipeableTextMobileStepper>
-			</Box> */}
-			<Container className={Styles.index__carousel} maxWidth="md">
-				
-				<SubHeader  
-						title="Próximos eventos"  
-						subtitle="Descubre lo que pasará proximamente en Belcorp"  
-						button="Ver talleres"  />
-				<Box my={4}>
-					<Grid  container justify="space-between" spacing={2}  >
-						{	
-							data.carousel.edges.map( edge => ( <HomeCourse key={edge.node.id}  edge={edge} />))
-						}
-					</Grid>
-				</Box>
+		<Fragment>
+			<Layout>
+				<SEO title="Inicio" />
+				{/* <Box mb={4}>
+					<SwipeableTextMobileStepper></SwipeableTextMobileStepper>
+				</Box> */}
+				<Container maxWidth="md">
+					
+					<SubHeader  
+							title="Próximos eventos"  
+							subtitle="Descubre lo que pasará proximamente en Belcorp"  
+							button="Ver talleres"  ></SubHeader>
+					
+					<Box my={4}>
+						<Grid  container justify="space-between" spacing={2}  >
+							{	
+								data.carousel.edges.map( edge => ( <HomeCourse key={edge.node.id}  edge={edge} />))
+							}
+						</Grid>
+					</Box>
 
-				<SubHeader  
-					title="Próximas actividades"  
-					subtitle="¿Pizza Nights? ¿Meetups? Descubre todas las actividades aquí."  
-					button="Ver actividades"  />
+					<SubHeader  
+						title="Próximas actividades"  
+						subtitle="¿Pizza Nights? ¿Meetups? Descubre todas las actividades aquí."  
+						button="Ver actividades"  ></SubHeader>
 
-				<Box my={4} >
-					<Grid  container justify="space-between" spacing={5}  >
-						
-						<ActivityItem />
-						<ActivityItem />
-								
-					</Grid>
-				</Box>
-				
-				<SubHeader  
-					title="Puntajes Tec Valley "  
-					subtitle="¿Estás cerca a ganar ese after office?  ¡Aprovecha al máximo TecAcademy!"  
-					button="Ver puntajes"  />
+					<Box my={4} >
+						<Grid  container justify="space-between" spacing={5}  >
+							
+							<ActivityItem />
+							<ActivityItem />
+									
+						</Grid>
+					</Box>
+					
+					<SubHeader  
+						title="Puntajes Tec Valley "  
+						subtitle="¿Estás cerca a ganar ese after office?  ¡Aprovecha al máximo TecAcademy!"  
+						button="Ver puntajes"  ></SubHeader>
 
-				<Box position="relative" my={4} >
-					<Grid  container spacing={3}  >	
-						{
-							data.countries.edges.map(  edge => (  <GroupCard  data={edge.node}  />  )  )
-						}			
-					</Grid>
-				</Box>
-				
-			</Container>
-		</Layout>
+					<Box position="relative" my={4} >
+						<Grid  container spacing={3}  >	
+							{
+								data.countries.edges.map(  edge => (  <GroupCard  data={edge.node}  />  )  )
+							}			
+						</Grid>
+					</Box>
+					
+				</Container>
+			</Layout>
+		</Fragment>
 	);
 };
 export default IndexPage;
