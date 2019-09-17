7f1ae29641f34890eb6c5e899966c0cc3d432a92
Paulo Tejada <paulo.tejada@MacBook-Pro-de-admin.local>
2019-09-17 13:12:11 -0500

    group card

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,6 +19,8 @@ import HomeAcademy from '../components/home-academy'
 import rocket from '../assets/media/rocket.jpg'
 import SwipeableTextMobileStepper  from '../components/mobcarousel'
 import SubHeader from '../components/subheader'
+import GroupCard from '../components/group-card'
+import { useStaticQuery, graphql } from 'gatsby';
 
 const useStyles = makeStyles({
     banner: {
@@ -46,6 +48,49 @@ const useStyles = makeStyles({
     }
 });
 const IndexPage = () => {
+	const data = useStaticQuery(graphql`
+		query {
+			slider : allContentfulCourse(filter: { showHomeBanner: { eq: true } }) {
+				edges {
+					node {
+						title
+						image {
+							file {
+								url
+							}
+						}
+						exhibitor
+					}
+				}
+			},
+			carousel:allContentfulCourse(filter: { showHomeCarousel:{ eq: true }  }){
+				edges {
+					node {
+						id
+						title
+						slug
+						exhibitor
+					}
+				}
+			},
+			countries: allContentfulGroup (sort : {
+                fields: points,
+                order: DESC
+            }) {
+                edges {
+                    node {
+                        name
+                        points
+                        image {
+                            file {
+                                url
+                            }
+                        }
+                    }
+                }
+            }
+		}
+	`);
     const classes = useStyles();
     return (
         <Fragment>
@@ -60,14 +105,13 @@ const IndexPage = () => {
 								title="Próximos eventos"  
 								subtitle="Descubre lo que pasará proximamente en Belcorp"  
 								button="Ver talleres"  ></SubHeader>
-					<SubHeader  
-						title="Próximas actividades"  
-						subtitle="¿Pizza Nights? ¿Meetups? Descubre todas las actividades aquí."  
-						button="Ver actividades"  ></SubHeader>
-					<SubHeader  
-						title="Puntajes Tec Valley "  
-						subtitle="¿Estás cerca a ganar ese after office?  ¡Aprovecha al máximo TecAcademy!"  
-						button="Ver puntajes"  ></SubHeader>
+					<Box position="relative" my={4} >
+						<Grid  container spacing={3}  >	
+							{
+								data.countries.edges.map(  edge => (  <GroupCard  data={edge.node}  />  )  )
+							}			
+						</Grid>
+					</Box>
 				</Container>
             </Layout>
         </Fragment>
