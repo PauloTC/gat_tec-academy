224d40ca89b931a111a350d3280f58f42f07c94c
Paulo Tejada <paulo.tejada@MacBook-Pro-de-admin.local>
2019-09-18 11:19:12 -0500

    working on styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	siteMetadata: {
-		title: `Gatsby Default Starter`,
-		description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+		title: `Tec Academy`,
+		description: `TecAcademy es el espacio que nos permite desarrollar nuestras habilidades softs y hard, mediante talleres, meetups, pizza nights, after office, etc.`,
 		author: `@gatsbyjs`
 	},
 	plugins: [
